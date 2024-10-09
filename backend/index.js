f79141b9600f8a42f31b56fa7a9486300d2bc53e
Aryan Khandelwal <aryankhandelwal0127@gmail.com>
2024-10-09 22:36:39 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://note-nest-lovat.vercel.app'],
+  origin: ['https://serene-donut-5d3a9a.netlify.app/', 'https://note-nest-lovat.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
